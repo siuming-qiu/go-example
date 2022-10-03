d54d43e81210165fb22e5d62343aaca362adc190
siuming_yau <1090735004@qq.com>
2022-10-03 12:25:48 +0800

    feat: simpledict

---
+++ simpledict/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type DictReq struct {
+	TransType string `json:"trans_type"`
+	Source    string `json:"source"`
+}
+type DictRes struct {
+	Rc   int `json:"rc"`
+	Wiki struct {
+		KnownInLaguages int `json:"known_in_laguages"`
+		Description     struct {
+			Source string      `json:"source"`
+			Target interface{} `json:"target"`
+		} `json:"description"`
+		ID   string `json:"id"`
+		Item struct {
+			Source string `json:"source"`
+			Target string `json:"target"`
+		} `json:"item"`
+		ImageURL  string `json:"image_url"`
+		IsSubject string `json:"is_subject"`
+		Sitelink  string `json:"sitelink"`
+	} `json:"wiki"`
+	Dictionary struct {
+		Prons struct {
+			EnUs string `json:"en-us"`
+			En   string `json:"en"`
+		} `json:"prons"`
+		Explanations []string      `json:"explanations"`
+		Synonym      []string      `json:"synonym"`
+		Antonym      []string      `json:"antonym"`
+		WqxExample   [][]string    `json:"wqx_example"`
+		Entry        string        `json:"entry"`
+		Type         string        `json:"type"`
+		Related      []interface{} `json:"related"`
+		Source       string        `json:"source"`
+	} `json:"dictionary"`
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprint(os.Stderr, "please input a word")
+		os.Exit(1)
+	}
+	word := os.Args[1]
+	query(word)
+}
+func query(word string) {
+	client := &http.Client{}
+	reqPrams := DictReq{TransType: "en2zh", Source: word}
+	buf, err := json.Marshal(reqPrams)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := bytes.NewReader(buf)
+	req, err := http.NewRequest("POST", "https://api.interpreter.caiyunai.com/v1/dict", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("authority", "api.interpreter.caiyunai.com")
+	req.Header.Set("accept", "application/json, text/plain, */*")
+	req.Header.Set("accept-language", "zh-CN,zh;q=0.9")
+	req.Header.Set("app-name", "xy")
+	req.Header.Set("content-type", "application/json;charset=UTF-8")
+	req.Header.Set("origin", "https://fanyi.caiyunapp.com")
+	req.Header.Set("os-type", "web")
+	req.Header.Set("referer", "https://fanyi.caiyunapp.com/")
+	req.Header.Set("sec-ch-ua", `"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"`)
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
+	req.Header.Set("sec-fetch-dest", "empty")
+	req.Header.Set("sec-fetch-mode", "cors")
+	req.Header.Set("sec-fetch-site", "cross-site")
+	req.Header.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
+	req.Header.Set("x-authorization", "token:qgemv4jr1y38jyq6vhvi")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	var dictRes DictRes
+	err = json.Unmarshal(bodyText, &dictRes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, item := range dictRes.Dictionary.Explanations {
+		fmt.Println(item)
+	}
+}
